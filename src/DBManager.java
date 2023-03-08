76c15c8f7e9607a13eee6e8cd129e541f8e677ae
blnc2 <blnc2@kent.ac.uk>
2023-03-08 22:17:56 +0000

    added login function

--- src/DBManager.java
+++ src/DBManager.java
@@ -12,21 +12,37 @@ public class DBManager {
         
 		DBManager db = new DBManager();		
 		db.testConnection();
+		System.out.println(db.Login("j.smith", "password2"));
 	}
 	
-	private void testConnection() {
+	public void testConnection() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/patients", "root", "groupa");// change to ur data base
 			statement = connection.createStatement();
-			resultSet = statement.executeQuery("SELECT * FROM patients");
-			while (resultSet.next()) {
-				System.out.println(resultSet.getString("id") + " - " + resultSet.getString("name") + " - " + resultSet.getString("age")
-				+ " - " + resultSet.getString("gender") + " - " + resultSet.getString("phone") + " - " + resultSet.getString("email"));
-			}
+			// resultSet = statement.executeQuery("SELECT * FROM patients");
+			// while (resultSet.next()) {
+			// 	System.out.println(resultSet.getString("id") + " - " + resultSet.getString("name") + " - " + resultSet.getString("age")
+			// 	+ " - " + resultSet.getString("gender") + " - " + resultSet.getString("phone") + " - " + resultSet.getString("email"));
+			// }
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+	}
+
+	public boolean Login(String username, String password) {
+		try {
+			resultSet = statement.executeQuery("SELECT username FROM users WHERE username = '" + username + "' AND password = '"+password+"'");
+			if (resultSet.next()) {
+				return true;
+			}
+		} catch (Exception e) {
+			return false;		
+		}
+
+		return false;
 		
 	}
 }
