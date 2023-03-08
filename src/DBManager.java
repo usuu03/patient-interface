93929e67be3d8f7dd3f08e9dcbe515e4e9ace2ea
blnc2 <blnc2@kent.ac.uk>
2023-03-08 16:43:04 +0000

    moved DBManager to src/DBManager.java

--- src/DBManager.java
+++ src/DBManager.java
@@ -1,5 +1,3 @@
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,18 +9,21 @@ public class DBManager {
 	private ResultSet resultSet;
 
 	public static void main(String[] args) {
+        
 		DBManager db = new DBManager();		
 		db.testConnection();
 	}
 	
 	private void testConnection() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/co559?user=***&password=***");
+			//Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/assignment1", "root", "Rasengan6!");
 			statement = connection.createStatement();
-			resultSet = statement.executeQuery("select * from assessments");
-			while (resultSet.next())
-				System.out.println(resultSet.getString("ModuleID") + " - " + resultSet.getString("AssessmentID") + " - " + resultSet.getString("AssessmentDescription"));
+			resultSet = statement.executeQuery("SELECT * FROM patients");
+			while (resultSet.next()) {
+				System.out.println(resultSet.getString("id") + " - " + resultSet.getString("name") + " - " + resultSet.getString("age")
+				+ " - " + resultSet.getString("gender") + " - " + resultSet.getString("phone") + " - " + resultSet.getString("email"));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
