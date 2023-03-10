39401f3827900786b44cbffe797a27d5cb132ed3
blnc2 <blnc2@kent.ac.uk>
2023-03-10 23:39:54 +0000

    Added db to gp gui

--- src/DBManager.java
+++ src/DBManager.java
@@ -3,6 +3,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.print.attribute.standard.RequestingUserName;
+
 public class DBManager {
 	private Connection connection;
 	private Statement statement;
@@ -12,21 +14,56 @@ public class DBManager {
         
 		DBManager db = new DBManager();		
 		db.testConnection();
+		System.out.println(db.Login("j.smith", "password2"));
 	}
 	
-	private void testConnection() {
+	public void testConnection() {
 		try {
-			//Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/assignment1", "root", "Rasengan6!");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/patients", "root", "groupa");// change to ur data base
 			statement = connection.createStatement();
-			resultSet = statement.executeQuery("SELECT * FROM patients");
-			while (resultSet.next()) {
-				System.out.println(resultSet.getString("id") + " - " + resultSet.getString("name") + " - " + resultSet.getString("age")
-				+ " - " + resultSet.getString("gender") + " - " + resultSet.getString("phone") + " - " + resultSet.getString("email"));
-			}
+			// resultSet = statement.executeQuery("SELECT * FROM patients");
+			// while (resultSet.next()) {
+			// 	System.out.println(resultSet.getString("id") + " - " + resultSet.getString("name") + " - " + resultSet.getString("age")
+			// 	+ " - " + resultSet.getString("gender") + " - " + resultSet.getString("phone") + " - " + resultSet.getString("email"));
+			// }
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+	}
+
+	public boolean Login(String username, String password) {
+		try {
+			resultSet = statement.executeQuery("SELECT username FROM users WHERE username = '" + username + "' AND password = '"+password+"'");
+			if (resultSet.next()) {
+				return true;
+			}
+		} catch (Exception e) {
+			return false;		
+		}
+
+		return false;
+		
+	}
+
+	public void registerPatient(String name, String email, String phone, String gender, String age){
+		try {
+
+            // Create a statement to insert the patient data into the patients table
+            String query = "INSERT INTO patients (name, age, gender, phone, email) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = (PreparedStatement) connection.prepareStatement(query);
+            statement.setString(1, name);
+            statement.setString(2, age);
+            statement.setString(3, gender);
+            statement.setString(4, phone);
+            statement.setString(5, email);
+
+            // Execute the insert statement
+            statement.executeUpdate();
+		} catch (Exception e) {
+			return;
+		}
 	}
 }
